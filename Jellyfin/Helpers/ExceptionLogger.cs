b05d746d467f35ef84a8e7ce6064f5a8ec733e25
Eric D. Rohler <erohler@progress.com>
2021-09-20 18:33:49 -0400

    initial commit

---
+++ Jellyfin/Helpers/ExceptionLogger.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jellyfin.Helpers
+{
+    public static class ExceptionLogger
+    {
+        private static bool _isWriting;
+        private static readonly string AppDataFolder;
+
+        static ExceptionLogger()
+        {
+            AppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        }
+
+        public static Task LogExceptionAsync(this Exception exception)
+        {
+            LogException(exception);
+
+            return Task.CompletedTask;
+        }
+
+        public static void LogException(this Exception exception)
+        {
+            if (_isWriting)
+            {
+                return;
+            }
+
+            var logContent = BuildLogMessage(exception);
+            var fileName = "ErrorLog" + "_" + DateTime.Today.ToString("yyyyMMdd") + "." + "log";
+            var filePath = Path.Combine(AppDataFolder, fileName);
+
+            StorageHelpers.AppendToLogFile(logContent, filePath);
+
+            PurgeOldLogFiles();
+
+            _isWriting = false;
+
+        }
+
+        private static string BuildLogMessage(Exception currentException)
+        {
+            // Build log file text
+            var messageBuilder = new StringBuilder();
+
+            try
+            {
+                messageBuilder.AppendLine("-----------------------------------------------------------------");
+                messageBuilder.AppendLine("Source: " + currentException.Source.Trim());
+                messageBuilder.AppendLine("Date Time: " + DateTime.Now);
+                messageBuilder.AppendLine("-----------------------------------------------------------------");
+                messageBuilder.AppendLine("Method: " + currentException.Message.Trim());
+                messageBuilder.AppendLine("Exception :: " + currentException);
+
+                if (currentException.InnerException != null)
+                {
+                    messageBuilder.AppendLine("InnerException :: " + currentException.InnerException);
+                }
+
+                messageBuilder.AppendLine(" ");
+            }
+            catch
+            {
+                messageBuilder.AppendLine("Exception:: Unknown Exception.");
+            }
+
+            return messageBuilder.ToString();
+        }
+
+        public static void PurgeOldLogFiles()
+        {
+            // Delete any outdated log files
+            try
+            {
+                var daysToKeepLog = 30;
+
+                var filePaths = Directory.GetFiles(AppDataFolder, "*.log").ToList();
+
+                if (filePaths.Count < 1)
+                    return;
+
+                foreach (var filePath in filePaths)
+                {
+                    DateTime created = File.GetCreationTime(filePath);
+                    if (DateTime.Compare(DateTime.Today.Date, created.AddDays(daysToKeepLog).Date) >= 0) File.Delete(filePath);
+                }
+            }
+            catch (Exception)
+            {
+                //#if DEBUG
+                //                Debugger.Break();
+                //#endif
+            }
+        }
+    }
+}
---
+++ Jellyfin/Views/ShellPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Jellyfin.Helpers;
+using Jellyfin.Sdk;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Jellyfin.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShellPage : Page
+    {
+        public ShellPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private string JellyfinSettingsFile = $"{ApplicationData.Current.LocalFolder.Path}\\Jellyfin-UWP-{Environment.MachineName}.json";
+        private string ClientName = "Jellyfin Universal Windows";
+        // TODO: Read this from a build config.
+        private string ClientVersion = "0.0.1";
+        private string DeviceName = Environment.MachineName;
+        private string DeviceId;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear User's Credentials
+            StorageHelpers.Instance.DeleteToken(DeviceName);
+            this.ContentFrame.Navigate(typeof(LoginPage));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            // Store the SdkSettings Locally
+            if (!File.Exists(JellyfinSettingsFile))
+            {
+
+                // First Run
+                DeviceId = Guid.NewGuid().ToString();
+
+                JObject sdkSettings = new JObject(
+                    new JProperty(nameof(ClientName), ClientName),
+                    new JProperty(nameof(ClientVersion), ClientVersion),
+                    new JProperty(nameof(DeviceName), DeviceName),
+                    new JProperty(nameof(DeviceId), DeviceId));
+
+                File.WriteAllText(JellyfinSettingsFile, sdkSettings.ToString());
+            }
+
+            App.ClientSettingsSdk = new SdkClientSettings();
+            App.ClientSettingsSdk.InitializeClientSettings(ClientName, ClientVersion, DeviceName, DeviceId);
+
+
+            // Get Access Token 
+            // If that exists then nav to MainPage
+            if (string.IsNullOrEmpty(StorageHelpers.Instance.LoadToken(DeviceName)))
+            {
+                // if Not then login
+                this.ContentFrame.Navigate(typeof(LoginPage));
+            } else
+            {
+                this.ContentFrame.Navigate(typeof(MainPage));
+            }
+
+        }
+    }
+}
