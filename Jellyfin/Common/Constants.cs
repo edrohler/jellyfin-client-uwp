bba71623117ebadcc16d69bceb079093a91d597d
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-09-21 10:52:05 -0400

    Optimized app launch workflow
    This commit also includes:
    - Work to optimize loading or creating SdkClientSettings
    - Changed the encryption private key
    - Created Contants.cs for app-wide constants

---
+++ Jellyfin/Common/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.Storage;
+
+namespace Jellyfin.Common
+{
+    public static class Constants
+    {
+        public static string JellyfinSettingsFile { get; } = $"{ApplicationData.Current.LocalFolder.Path}\\Jellyfin-UWP-{Environment.MachineName}.json";
+
+        public static string AppVersion { get; } = $"{Package.Current.Id.Version.Major}.{Package.Current.Id.Version.Minor}.{Package.Current.Id.Version.Build}";
+
+        public static string AppName { get; } = Package.Current.DisplayName;
+
+        public static string DeviceName { get; } = Environment.MachineName;
+    }
+}
--- Jellyfin/Views/ShellPage.xaml.cs
+++ Jellyfin/Views/ShellPage.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Jellyfin.Common;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -30,48 +31,21 @@ namespace Jellyfin.Views
         {
             this.InitializeComponent();
         }
-
-        private string JellyfinSettingsFile = $"{ApplicationData.Current.LocalFolder.Path}\\Jellyfin-UWP-{Environment.MachineName}.json";
-        private string ClientName = "Jellyfin Universal Windows";
-        // TODO: Read this from a build config.
-        private string ClientVersion = "0.0.1";
-        private string DeviceName = Environment.MachineName;
-        private string DeviceId;
-
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             // Clear User's Credentials
-            StorageHelpers.Instance.DeleteToken(DeviceName);
+            StorageHelpers.Instance.DeleteToken(Constants.DeviceName);
             ContentFrame.Navigate(typeof(LoginPage));
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-
-            // Store the SdkSettings Locally
-            if (!File.Exists(JellyfinSettingsFile))
-            {
-
-                // First Run
-                DeviceId = Guid.NewGuid().ToString();
-
-                JObject sdkSettings = new JObject(
-                    new JProperty(nameof(ClientName), ClientName),
-                    new JProperty(nameof(ClientVersion), ClientVersion),
-                    new JProperty(nameof(DeviceName), DeviceName),
-                    new JProperty(nameof(DeviceId), DeviceId));
-
-                File.WriteAllText(JellyfinSettingsFile, sdkSettings.ToString());
-            }
-
-            App.ClientSettingsSdk = new SdkClientSettings();
-            App.ClientSettingsSdk.InitializeClientSettings(ClientName, ClientVersion, DeviceName, DeviceId);
-
-
+            
             // Get Access Token 
             // If that exists then nav to MainPage
-            if (string.IsNullOrEmpty(StorageHelpers.Instance.LoadToken(DeviceName)))
+            if (string.IsNullOrEmpty(StorageHelpers.Instance.LoadToken(Constants.DeviceName)))
             {
                 // if Not then login
                 ContentFrame.Navigate(typeof(LoginPage));
