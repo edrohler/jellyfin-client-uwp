289fdbe407e7d59672db0d0754c1ebef19354773
Eric D. Rohler <erohler@progress.com>
2021-10-15 23:13:45 -0400

    move sortby and sortorder collection instantiation

--- src/Jellyfin.Uwp/Views/ItemsPage.xaml.cs
+++ src/Jellyfin.Uwp/Views/ItemsPage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using Jellyfin.Models;
+using Jellyfin.Models.Enums;
+using Jellyfin.Sdk;
 using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -13,21 +18,96 @@ namespace Jellyfin.Views
             InitializeComponent();
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
             // Get UserView
             ViewModel.UserView = App.Current.UserViews.Items.FirstOrDefault(x => x.Id == (Guid)e.Parameter);
 
-            // Set Initial SortBy and SortOrder
+            // Instantiate SortOrderCollection
+            foreach (object item in Enum.GetValues(typeof(SortOrder)))
+            {
+                ViewModel.SortOrderCollection.Add(new SortDataItem
+                {
+                    DisplayName = item.ToString(),
+                    Value = item.ToString()
+                });
+            }
+
+            // Instantiate SortByCollection
             switch (ViewModel.UserView.CollectionType)
             {
                 case "movies":
+                    // Instantiate SortByCollection
+                    foreach (object item in Enum.GetValues(typeof(MoviesSortBy)))
+                    {
+                        ViewModel.SortByCollection.Add(new SortDataItem
+                        {
+                            DisplayName = item.GetType()
+                                .GetField(item.ToString())
+                                .GetCustomAttribute<DisplayNameAttribute>()
+                                .DisplayName,
+                            Value = item.ToString()
+                        });
+                    }
+
+                    ViewModel.SortOrder = new SortOrder[] { SortOrder.Descending };
+                    ViewModel.SortBy = new string[] { "DateCreated", "SortName", "ProductionYear" };
                     SortByList.SelectedItem = ViewModel.SortByCollection.FirstOrDefault(i => i.Value == "SortName");
                     SortOrderList.SelectedItem = ViewModel.SortOrderCollection.FirstOrDefault(i => i.Value == "Descending");
                     break;
+                case "tvshows":
+                    foreach (object item in Enum.GetValues(typeof(TvShowsSortBy)))
+                    {
+                        ViewModel.SortByCollection.Add(new SortDataItem
+                        {
+                            DisplayName = item.GetType()
+                                .GetField(item.ToString())
+                                .GetCustomAttribute<DisplayNameAttribute>()
+                                .DisplayName,
+                            Value = item.ToString()
+                        });
+                    }
+
+                    ViewModel.SortOrder = new SortOrder[] { SortOrder.Ascending };
+                    ViewModel.SortBy = new string[] { "SortName" };
+                    SortByList.SelectedItem = ViewModel.SortByCollection.FirstOrDefault(i => i.Value == "SortName");
+                    SortOrderList.SelectedItem = ViewModel.SortOrderCollection.FirstOrDefault(i => i.Value == "Ascending");
+                    break;
+                case "music":
+                    foreach (object item in Enum.GetValues(typeof(MusicSortBy)))
+                    {
+                        ViewModel.SortByCollection.Add(new SortDataItem
+                        {
+                            DisplayName = item.GetType()
+                                .GetField(item.ToString())
+                                .GetCustomAttribute<DisplayNameAttribute>()
+                                .DisplayName,
+                            Value = item.ToString()
+                        });
+                    }
+
+                    ViewModel.SortOrder = new SortOrder[] { SortOrder.Ascending };
+                    ViewModel.SortBy = new string[] { "SortName" };
+                    SortByList.SelectedItem = ViewModel.SortByCollection.FirstOrDefault(i => i.Value == "SortName");
+                    SortOrderList.SelectedItem = ViewModel.SortOrderCollection.FirstOrDefault(i => i.Value == "Ascending");
+                    break;
                 default:
+                    foreach (object item in Enum.GetValues(typeof(FolderSortBy)))
+                    {
+                        ViewModel.SortByCollection.Add(new SortDataItem
+                        {
+                            DisplayName = item.GetType()
+                                .GetField(item.ToString())
+                                .GetCustomAttribute<DisplayNameAttribute>()
+                                .DisplayName,
+                            Value = item.ToString()
+                        });
+                    }
+
+                    ViewModel.SortOrder = new SortOrder[] { SortOrder.Ascending };
+                    ViewModel.SortBy = new string[] { "IsFolder", "SortName" };
                     SortByList.SelectedItem = ViewModel.SortByCollection.FirstOrDefault(i => i.Value == "SortName");
                     SortOrderList.SelectedItem = ViewModel.SortOrderCollection.FirstOrDefault(i => i.Value == "Ascending");
                     break;
